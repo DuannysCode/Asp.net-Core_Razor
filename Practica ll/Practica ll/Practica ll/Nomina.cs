82f4f6d293d0682372f3875c5e291c191f6951e0
DuannysCode <richardduannys@outlook.com>
2021-08-31 17:46:17 -0400

    Tarea Practica II

---
+++ Practica ll/Practica ll/Practica ll/Nomina.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica_ll
+{
+    public class Nomina
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cargo { get; set; }
+        public double Sueldo_Bruto { get; set; }
+        //public static double SFS { get; set; }
+
+        public double SFS(double sueldo_bruto)
+        {
+            double sfs = 0.0304;
+            double descuento = sueldo_bruto * sfs;
+
+            return Math.Round(descuento, 2);
+        }
+
+        public double AFP(double sueldo_bruto)
+        {
+            double afp = 0.0287;
+            double descuento = sueldo_bruto * afp;
+
+            return Math.Round(descuento, 2);
+        }
+
+        public double ISR(double sfs, double afp, double sueldo_bruto)
+        {
+            double deducciones = sfs + afp;
+            double ISR = (sueldo_bruto - deducciones)*12;
+            double Escala1 = 416220.00;
+            double Escala2 = 416220.01;
+            double Escala3 = 624329.00;
+            double Escala4 = 624329.01;
+            double Escala5 = 867123.00;
+            double Escala6 = 867123.01;
+            double porcentaje1 = 0.15;
+            double porcentaje2 = 0.20;
+            double porcentaje3 = 0.25;
+            double monto_fijo1 = 31216.00;
+            double monto_fijo2 = 79776.00;
+            double isr = 0.0;
+            double descuento = 0.0;
+
+            if (ISR <= Escala1)
+            {
+                ISR = 0.0;
+            }
+            else if(ISR>=Escala2 && ISR<=Escala3)
+            {
+                isr = ISR - Escala2;
+                descuento = isr * porcentaje1;
+                Math.Round(ISR = descuento / 12, 2);
+            }
+            else if(ISR>=Escala4 && ISR<=Escala5)
+            {
+                isr = ISR - Escala4;
+                descuento = isr * porcentaje2;
+                double monto_fijo = descuento + monto_fijo1;
+                ISR = monto_fijo / 12;
+            }
+            else if (ISR >= Escala6)
+            {
+                isr = ISR - Escala6;
+                descuento = isr * porcentaje3;
+                double monto_fijo = descuento + monto_fijo2;
+                ISR = monto_fijo / 12;
+            }
+
+            return Math.Round(ISR, 2);
+        }
+
+        public double Total_TSS(double sfs, double afp)
+        {
+            double tss = sfs + afp;
+            return Math.Round(tss, 2);
+        }
+
+        public double Total_Desc(double total_tss, double isr)
+        {
+            double total_desc = total_tss + isr;
+            return Math.Round(total_desc, 2);
+        }
+
+        public double Sueldo_Neto(double sueldo_bruto, double sfs, double afp, double isr)
+        {
+            double sueldo_neto = sueldo_bruto - (sfs + afp + isr);
+            return Math.Round(sueldo_neto, 2);
+        }
+    }
+}
--- Practica ll/Practica ll/Practica ll/Pages/Nomina.cshtml.cs
+++ Practica ll/Practica ll/Practica ll/Pages/Nomina.cshtml.cs
@@ -9,25 +9,19 @@ namespace Practica_ll.Pages
 {
     public class NominaModel : PageModel
     {
-        public List<string> Nombres { get; set; } = new List<string>() { "Elianny", "Maria", "Richard", "Amanda", "Alejandro" };
-        public List<string> Apellidos { get; set; } = new List<string>() { "Arias", "Batista", "Arias", "Paradis", "Cuevas" };
-        public List<string> Cargo { get; set; } = new List<string>() { "Enc. Contabilidad", "Diseñadora Grafica", "IT Manager", "Secretaria", "Chofer" };
-        public List<double> Salario { get; set; } = new List<double>() { 100000.00, 90000.00, 275000.00, 18000.00, 35000.00 };
+        public List<Nomina> Nominas { get; set; }
 
-        //Funcion SFS
-        public double SFS(List<double> salarios)
-        {
-            double sfs = 0.0304;
-            double descuento = 0.0;
-            int id = 0;
-            while (id < 5)
+        public NominaModel()
             {
-                descuento = salarios[id] * sfs;
-                id++;
-            }
-            return descuento;
-        }
+            this.Nominas = new List<Nomina>() {
+                new Nomina() {Nombre = "Richard", Apellido = "Arias", Cargo = "Senior Developer III", Sueldo_Bruto = 245000 },
+                new Nomina() {Nombre = "Elianny", Apellido = "Arias", Cargo = "Enc. Contabilidad", Sueldo_Bruto = 95000 },
+                new Nomina() {Nombre = "Maria", Apellido = "Batista", Cargo = "Publicista", Sueldo_Bruto = 45000 },
+                new Nomina() {Nombre = "Alejandro", Apellido = "Paradis", Cargo = "Chofer", Sueldo_Bruto = 30000 },
+                new Nomina() {Nombre = "Amanda", Apellido = "Encarnacion", Cargo = "Conserje", Sueldo_Bruto = 21000 }
+            };
 
+            }
         public void OnGet()
         {
 
